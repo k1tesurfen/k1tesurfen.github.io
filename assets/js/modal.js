72061d833697d5e0623f767631bd808ed109b171
Kaith Menken <kaithmenken@gmail.com>
2022-10-24 01:07:00 +0200

    about page and back to top button

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -23,3 +23,8 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+function topFunction(){
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
